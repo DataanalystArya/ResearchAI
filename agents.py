446851503e69360a50dec110d782dd3d9f427452
Kanishka Singh <kanishkasingh@Kanishkas-MacBook-Air.local>
2026-07-12 21:10:14 +0530

    added agents.py

---
+++ agents.py
@@ -0,0 +1,17 @@
+from langchain.agents import create_agent
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from tools import web_search, scrape_url
+from dotenv import load_dotenv
+
+load_dotenv()
+
+#model setup
+llm = ChatOpenAI(model="gpt-4o-mini", temperature=0)
+#first agent 
+def build_search_agent():
+    return create_agent(
+        model=llm
+        tools=[web_search]
+    )
